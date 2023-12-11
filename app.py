0a72d2474cbf99a806e29e4dca46813921fe800c
pragati0103 <pragatikomarla@gmail.com>
2023-12-11 23:20:49 +0530

    integrated html with flask api

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask import Flask, render_template, request, redirect, url_for, jsonify, session
 from flask_sqlalchemy import SQLAlchemy
+from flask_session import Session
+from tempfile import mkdtemp
+from datetime import datetime 
+from functools import wraps
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'  # SQLite URI
@@ -20,8 +24,7 @@ class Product(db.Model):
     description = db.Column(db.Text)
     price = db.Column(db.Float, nullable=False)
     image_url = db.Column(db.String(255))
-    stock_quantity = db.Column(db.Integer)
-    manufacturer = db.Column(db.String(50))
+    carts = db.relationship('Cart', backref='product', lazy=True)
 
 class Order(db.Model):
     order_id = db.Column(db.Integer, primary_key=True)
@@ -44,11 +47,16 @@ class Cart(db.Model):
     date_added = db.Column(db.DateTime, default=db.func.current_timestamp())
 
 class Category(db.Model):
-    category_id = db.Column(db.Integer, primary_key=True)
-    categoty_name = db.Column(db.String(50), unique=True, nullable=False)
+    category_id = db.Column(db.Integer,nullable=False )
+    categoty_name = db.Column(db.String(50), nullable=False)
     product_id = db.Column(db.Integer, db.ForeignKey('product.product_id'), nullable=False)
-    
 
+    __table_args__ = (
+        db.PrimaryKeyConstraint('category_id', 'product_id'),
+    )
+
+    
+#not needed ig?
 class Review(db.Model):
     review_id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), nullable=False)
@@ -57,10 +65,45 @@ class Review(db.Model):
     comment = db.Column(db.Text)
     date = db.Column(db.DateTime, default=db.func.current_timestamp())
 
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+
+Session(app)
+
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        # You should perform actual authentication here
+        # For testing purposes, let's check against a sample user
+        sample_user = User.query.filter_by(username=username, password_hash=password).first()
+
+        if sample_user:
+            # Store the user's ID in the session
+            session['user_id'] = sample_user.user_id
+            print(sample_user.username)
+            return render_template('index.html', username=sample_user.username)  # Redirect to the home page after successful login
+        else:
+            return render_template('login.html', error='Invalid credentials')
+
+    return render_template('login.html')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -82,11 +125,12 @@ def register():
     return render_template('register.html')
 
 @app.route('/products/<int:category_id>', methods=['GET'])
-def product_list(categoty_id):
+def product_list(category_id):
+    print(category_id)
     products = Product.query.all()
     return render_template('products.html', products=products)
 
-@app.route('/products/<int:product_id>', methods=['GET'])
+@app.route('/product/<int:product_id>', methods=['GET'])
 def show_product_details(product_id):
     product = Product.query.get(product_id)
 
@@ -98,9 +142,10 @@ def show_product_details(product_id):
 
 
 @app.route('/add_to_cart/<int:product_id>')
+@login_required
 def add_to_cart(product_id):
-    # For simplicity, assume a user is always logged in (user_id=1)
-    user_id = 1
+
+    user_id = session.get('user_id')
 
     # Check if the product is already in the user's cart
     existing_cart_item = Cart.query.filter_by(user_id=user_id, product_id=product_id).first()
@@ -114,6 +159,39 @@ def add_to_cart(product_id):
     db.session.commit()
 
     return redirect(url_for('product_list'))
+
+@app.route('/cart', methods=['GET'])
+@login_required
+def cart():
+    # Retrieve the user ID from the session
+    user_id = session.get('user_id')
+
+   
+    # Fetch the user's cart details from the database
+    cart_items = Cart.query.filter_by(user_id=user_id).all()
+
+    # Create a list of cart items with details
+    cart_details = [
+        {
+            'cart_id': item.cart_id,
+            'product_id': item.product_id,
+            'quantity': item.quantity,
+            'date_added': item.date_added.strftime('%Y-%m-%d %H:%M:%S'),
+            'product_details': {
+                'name': item.product.name,
+                'price': item.product.price,
+                'image_url':item.product.image_url
+            }
+        }
+        for item in cart_items
+    ]
+
+    # Calculate the total price of items in the cart
+    total_price = sum(item['product_details']['price'] * item['quantity'] for item in cart_details)
+
+    # Render the carts.html template with cart details
+    return render_template('cart.html', cart_items=cart_details, total_price=total_price)
+
     
 
 if __name__ == '__main__':
