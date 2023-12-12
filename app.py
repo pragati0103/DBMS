8edd36114a3b74ab59f927a4300e1f71cbe5c1d7
pragati0103 <pragatikomarla@gmail.com>
2023-12-12 15:33:26 +0530

    orders path

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ class Review(db.Model):
 
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_FILE_DIR"] = mkdtemp()
-app.config["SESSION_PERMANENT"] = True
+app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 
 Session(app)
